779dcfd210a59ecbe7659f4df5cde6d113ab7a08
mhiskall282 <johnokyere282@icloud.com>
2025-01-02 14:25:46 +0000

    ggg

--- api/index.py
+++ api/index.py
@@ -22,27 +22,3 @@ def handler(event, context):
             </html>
         """
     }
-import os
-from subprocess import Popen
-
-def handler(event, context):
-    # Start the Streamlit app as a subprocess
-    process = Popen(["streamlit", "run", "app.py", "--server.port=8000", "--server.headless=true"])
-
-    # Return a response that points to the running Streamlit app
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/html"
-        },
-        "body": f"""
-            <html>
-                <head>
-                    <meta http-equiv="refresh" content="0; url=http://127.0.0.1:8000" />
-                </head>
-                <body>
-                    <p>If you are not redirected, click <a href="http://127.0.0.1:8000">here</a>.</p>
-                </body>
-            </html>
-        """
-    }
